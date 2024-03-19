e1adac2228786d8f4a7a91bbd709493e7d0a7cbd
Brenden Hartman <brendenhart22@gmail.com>
2024-03-19 12:58:48 -0500

    Added the correct event listeners andd function handlers to new dom elements in my components

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe(){
+    function handleBlur(event){
+        console.log("Hey! Eyes on me!");
+    };
+
+    function handleFocus(event){
+        console.log("Good!");
+    };
+
+    return(
+        <div>
+            <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+        </div>
+    );
+};
+export default EyesOnMe;
